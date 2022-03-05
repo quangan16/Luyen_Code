f976f5a8844be347aa9964becdb051a11decb38d
Nguyen An <nqan1610@gmail.com>
2022-03-05 16:06:15 +0700

    initial commit

---
+++ CB04.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+   int a, b, c;
+   cin>>a>>b;
+   cout<<a+b<<endl;
+   cout<<a-b<<endl;
+   cout<<a*b<<endl;
+   if(b==0)
+   {
+      cout<<"INF";
+   }
+   else{
+      cout<<fixed<<setprecision(2)<<(float)a/b;
+   }
+   return 0;
+}
